d758eaa9b64dcf571c28ca1e7856b9169fedd6a9
David Banks <dave@hoglet.com>
2016-11-28 14:31:29 +0000

    Unix line endings, spaces not tabs
    Change-Id: I7a7fe15ba0af1dbc43ca68a917cb9d2a8ea08e5c

--- TERM.asm
+++ TERM.asm
@@ -2,9 +2,9 @@
 ;TERMINAL
 ; Atomic Serial Port terminal by Kees van Oss 2012
 ;-------------------------------------------------
-	.DEFINE asm_code $3000
-	.DEFINE header   1		; Header Wouter Ras emulator
-	.DEFINE filenaam "TERM"
+        .DEFINE asm_code $3000
+        .DEFINE header   1              ; Header Wouter Ras emulator
+        .DEFINE filenaam "TERM"
 
 .org asm_code-22*header
 
@@ -13,15 +13,15 @@
 ; ATM Header for Atom emulator Wouter Ras
 
 name_start:
-	.byte filenaam			; Filename
+        .byte filenaam                  ; Filename
 name_end:
-	.repeat 16-name_end+name_start	; Fill with 0 till 16 chars
-	  .byte $0
-	.endrep
+        .repeat 16-name_end+name_start  ; Fill with 0 till 16 chars
+          .byte $0
+        .endrep
 
-	.word asm_code			; 2 bytes startaddress
-	.word exec			; 2 bytes linkaddress
-	.word eind_asm-start_asm	; 2 bytes filelength
+        .word asm_code                  ; 2 bytes startaddress
+        .word exec                      ; 2 bytes linkaddress
+        .word eind_asm-start_asm        ; 2 bytes filelength
 
 ;********************************************************************
 .ENDIF
@@ -29,5 +29,5 @@ name_end:
 
 exec:
 start_asm:
-	.include "terminal.inc"
+        .include "terminal.inc"
 eind_asm:
